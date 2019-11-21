6cc18d78c4771c368969075b7654ad75b834051b
kIshikura-F <kazuki.suka37@gmail.com>
2019-11-21 15:24:30 +0900

    S3導入（１）

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,9 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.2'
 
+#linked_filesにmaster.keyを追加
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 # Capistranoのログの表示に利用する
 set :application, 'freemarket_sample_63k'
 
