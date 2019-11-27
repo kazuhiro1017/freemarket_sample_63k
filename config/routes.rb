0e97a45fd1295eb39289c6cc03fa61e941518e57
T-mura-mura <takumu.kawamura@gmail.com>
2019-11-27 15:03:47 +0900

    registrationの画面複数
    registrationに画面を複数使うのに合わせて、ルーティング、コントローラ、ビューの設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
+    devise_scope :user do
+    get 'users/sign_up/user_info', to: 'users/registrations#user_add'
+    get 'users/sign_up/phone_info', to: 'users/registrations#phone_add'
+    get 'users/sign_up/address_info', to: 'users/registrations#address_add'
+    get 'users/sign_up/card_info', to: 'users/registrations#card_add'
+    get 'users/sign_up/complete', to: 'users/registrations#complete'
+  end
   root to: 'items#index'
 end
