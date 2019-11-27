8586ede5dff1c99abc4c50ded55f7090b361e8ed
T-mura-mura <takumu.kawamura@gmail.com>
2019-11-27 16:30:39 +0900

    scssファイル名変更と、progress headerの色を変えるjs

--- app/assets/javascripts/channels/user_registration.js
+++ app/assets/javascripts/channels/user_registration.js
@@ -1,7 +1,25 @@
 $(function(){
+
   let path = location.pathname.split("/")
-  let page = 4;
-  if (1 == 1){
+  let page = -1;
+  switch(path[3]) {
+    case "user_info":
+      page = 0;
+      break;
+    case "phone_info":
+      page = 1;
+      break;
+    case "address_info":
+      page = 2;
+      break;
+    case "card_info":
+      page = 3;
+      break;
+    case "complete":
+      page = 4;
+      break;
+  }
+  if (page !== -1) {
     $(".header-with-progress__nav__lists__li").eq(page).
     css('color', '#ea352d');
 
