4df3a3b073a6bf20200311f5653d4be72a0c68dd
T-mura-mura <takumu.kawamura@gmail.com>
2019-12-02 13:22:37 +0900

    binding.pry導入

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,14 +5,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def phone_add
+    @user = User.new
   end
 
   def address_add
   end
 
   def card_add
+    # params[:user][:birthday] = birthday_join
   end
 
   def complete
   end
+
+  # この辺のメソッドは、paramsにフォームの情報が入る、postの段階でないと働かない。
+  private 
+    def user_params
+      params.require(:user).permit(:birthday)
+    end
+
+    
+    def birthday_join
+      date = params[:user][:birthday]
+
+      if date["birthday(1i)"].empty? && date["birthday(2i)"].empty? && date["birthday(3i)"].empty?
+        return
+      end
+
+      Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
+    end
+
 end
