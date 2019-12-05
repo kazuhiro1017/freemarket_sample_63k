371fd75b1d9e0d062ad46597014a588ef0d577e1
kIshikura-F <kazuki.suka37@gmail.com>
2019-12-05 16:07:00 +0900

    enumの命名の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,13 +16,13 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
 
-  enum method: {
+  enum delivery_method: {
     "---":0, "未定":1, "らくらくメルカリ便":2, "ゆうメール":3, "レターパック":4,
     "普通郵便(定形、定形外)":5, "クロネコヤマト":6, "ゆうパック":7,
     "クリックポスト":8, "ゆうパケット":9
   }, _suffix: true
 
-  enum days: {
+  enum delivery_days: {
     "---":0, "1~2日で発送":1, "2~3日で発送":2, "4~7日で発送":3
   }, _suffix: true
 
@@ -41,7 +41,7 @@ class Item < ApplicationRecord
     "4XL(5L)以上":8, "FREE SIZE":9
   }, _suffix: true
 
-  enum prefecture: {
+  enum from_prefecture: {
     "---":0,北海道:1,青森県:2,岩手県:3,宮城県:4,秋田県:5,山形県:6,福島県:7,
     茨城県:8,栃木県:9,群馬県:10,埼玉県:11,千葉県:12,東京都:13,神奈川県:14,
     新潟県:15,富山県:16,石川県:17,福井県:18,山梨県:19,長野県:20,
@@ -51,5 +51,10 @@ class Item < ApplicationRecord
     徳島県:36,香川県:37,愛媛県:38,高知県:39,
     福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,沖縄県:47,未定:48
   }, _suffix: true
+
+  # def user
+  #   #インスタンスメソッドないで、selfはインスタンス自身を表す
+  #   return User.find_by(id: self.user_id)
+  # end
 
 end
