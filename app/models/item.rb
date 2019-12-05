695368151332734b76c1222786f0d20c13f1bb21
kazuhiro1017 <kazuhiro10170326@gmail.com>
2019-12-05 18:40:57 +0900

    added items to toppage

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,13 +16,13 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
 
-  enum method: {
+  enum delivery_method: {
     "---":0, "未定":1, "らくらくメルカリ便":2, "ゆうメール":3, "レターパック":4,
     "普通郵便(定形、定形外)":5, "クロネコヤマト":6, "ゆうパック":7,
     "クリックポスト":8, "ゆうパケット":9
   }, _suffix: true
 
-  enum days: {
+  enum delivery_days: {
     "---":0, "1~2日で発送":1, "2~3日で発送":2, "4~7日で発送":3
   }, _suffix: true
 
@@ -41,7 +41,7 @@ class Item < ApplicationRecord
     "4XL(5L)以上":8, "FREE SIZE":9
   }, _suffix: true
 
-  enum prefecture: {
+  enum from_prefecture: {
     "---":0,北海道:1,青森県:2,岩手県:3,宮城県:4,秋田県:5,山形県:6,福島県:7,
     茨城県:8,栃木県:9,群馬県:10,埼玉県:11,千葉県:12,東京都:13,神奈川県:14,
     新潟県:15,富山県:16,石川県:17,福井県:18,山梨県:19,長野県:20,
