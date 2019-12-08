d335586de724a5cc834db4bbf085a525fb601534
T-mura-mura <takumu.kawamura@gmail.com>
2019-12-08 18:41:37 +0900

    バリデーション素案
    とりあえず書いてみた。通るかどうか、テストはまだ。

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,8 @@
 class Card < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :card_number, presence: true
+  validates :card_number, format: { with: /\d{,16}/}, presence: true
   validates :expiry_date, presence: true
-  validates :security_code, presence: true
+  validates :security_code, format: { with: /\d{3,4}/}, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,22 @@ class User < ApplicationRecord
   has_one :card, dependent: :destroy
   accepts_nested_attributes_for :card
 
-  kanji = /\A[一-龥]+\z/
+#  kanji = /\A[一-龥]+\z/
+  zenkaku = /\A[ぁ-んァ-ン一-龥]+\z/
   kana = /\A([ァ-ン]|ー)+\z/
+  pass = /\A[a-z\d]{7,100}\z/i
+#  email = /\A([^@\s]+)@[^@\s]+\z/
+  email = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  phone = /\d{,11}/
 
 
   validates :nickname, presence: true, length: { maximum: 20 }
-  validates :password, format: { with: /[a-z\d]{7,100}/i}, presence: true
-  validates :first_name, presence: true, length: { maximum: 15 }, format: { with: kanji }
-  validates :last_name, presence: true, length: { maximum: 15 }, format: { with: kanji }
+  validates :password, format: { with: pass }, presence: true
+  validates :first_name, presence: true, length: { maximum: 15 }, format: { with: zenkaku }
+  validates :last_name, presence: true, length: { maximum: 15 }, format: { with: zenkaku }
   validates :first_name_kana, presence: true, length: { maximum: 15 }, format: { with: kana }
   validates :last_name_kana, presence: true, length: { maximum: 15 }, format: { with: kana }
   validates :birthday, presence: true
-  validates :phone_number, presence: true
-  validates :email, uniqueness: true, presence: true
+  validates :phone_number, format: { with: phone }, presence: true
+  validates :email, uniqueness: true, format: { with: email }, presence: true
 end
