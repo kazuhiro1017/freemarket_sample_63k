54b009118796fbf324aaae6bef9daf02c617f422
T-mura-mura <takumu.kawamura@gmail.com>
2019-12-09 20:04:55 +0900

    パンくず用ファイルに、特定の
    コントローラとアクションが動いた時の（パンくず用）メソッドを記述。
    あと、ログアウト用のリンクを修正。

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -3,11 +3,27 @@ crumb :root do
 end
 
 crumb :users_show do
-  link "マイページ", show_user_path(user_id: session[:user_id])
+  link "マイページ", user_path(id: session[:user_id])
   parent :root
 end
 
 crumb :users_profile do
   link "プロフィール"
   parent :users_show
+end
+
+crumb :users_signout do
+  link "ログアウト"
+  parent :users_show
+end
+
+crumb :users_card do
+  link "支払い方法"
+  parent :users_show
+end
+
+crumb :items_show do
+  item = Item.find_by(id: params[:id])
+  link "#{item.name}"
+  parent :root
 end
