4d7c96318c82f020cce6152ce6cdb09fa78989e3
T-mura-mura <takumu.kawamura@gmail.com>
2019-12-09 12:53:50 +0900

    テストコードを日本語エラーバージョンに変更

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -16,37 +16,37 @@ RSpec.describe Address, type: :model do
       it 'is invalid without post_number' do
         address = build(:address, post_number: nil)
         address.valid?
-        expect(address.errors[:post_number]).to include("can't be blank")
+        expect(address.errors[:post_number]).to include("を入力してください")
       end
 
       it 'is invalid without prefecture' do
         address = build(:address, prefecture: "---")
         address.valid?
-        expect(address.errors[:prefecture]).to include("is reserved")
+        expect(address.errors[:prefecture]).to include("は予約されています")
       end
 
       it 'is invalid without city' do
         address = build(:address, city: nil)
         address.valid?
-        expect(address.errors[:city]).to include("can't be blank")
+        expect(address.errors[:city]).to include("を入力してください")
       end
 
       it 'is invalid without address' do
         address = build(:address, address: nil)
         address.valid?
-        expect(address.errors[:address]).to include("can't be blank")
+        expect(address.errors[:address]).to include("を入力してください")
       end
 
       it 'is invalid as no "-" between post_number' do
         address = build(:address, post_number: "1234567")
         address.valid?
-        expect(address.errors[:post_number]).to include("is invalid")
+        expect(address.errors[:post_number]).to include("は不正な値です")
       end
 
       it 'is invalid as alphabets in post_number' do
         address = build(:address, post_number: "12a-4567")
         address.valid?
-        expect(address.errors[:post_number]).to include("is invalid")
+        expect(address.errors[:post_number]).to include("は不正な値です")
       end
 
     end
