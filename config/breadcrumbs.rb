3c68d6bd13044e4e59ec297799e14d2682836235
T-mura-mura <takumu.kawamura@gmail.com>
2019-12-11 17:12:20 +0900

    新しいページに対してパンくず機能の調整

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -22,8 +22,18 @@ crumb :users_card do
   parent :users_show
 end
 
+crumb :users_identification do
+  link "本人情報ページ"
+  parent :users_show
+end
+
 crumb :items_show do
   item = Item.find_by(id: params[:id])
   link "#{item.name}"
   parent :root
+end
+
+crumb :creditcard_card do
+  link "支払い方法"
+  parent :users_show
 end
