cdf8dabee5ca6cc5c0e9f1b92498cbb7ed0b4c54
kIshikura-F <kazuki.suka37@gmail.com>
2019-12-11 20:27:05 +0900

    本番環境のみでBasic認証をする

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,10 @@ server '52.68.113.141', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+server "52.68.113.141", user: "ec2-user", roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 
 # role-based syntax
